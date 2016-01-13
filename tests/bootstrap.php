8484b64c3e27b11f1289b14c6bf6c622d46d4922
Dion Hulse <contact@dd32.id.au>
2016-01-13 22:24:42 +1100

    Adding initial tests

---
+++ tests/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+$dir = dirname( dirname( __FILE__ ) ) . '/';
+
+include $dir . 'lib/class-ginger-mo.php';
+include $dir . 'lib/class-ginger-mo-translation-file.php';
+
+define( 'GINGER_MO_TEST_DATA', $dir . 'tests/data/' );
