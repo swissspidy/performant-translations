559f4cf68b56d0e5b14f4c5df3b42b8aac73037c
Pascal Birchler <pascalb@google.com>
2023-06-29 17:05:35 +0200

    Add separate object cache column

--- tests/performance/specs/block-theme.ts
+++ tests/performance/specs/block-theme.ts
@@ -13,8 +13,7 @@ enum Scenario {
 	Default = 'Default',
 	GingerMo = 'Ginger-MO (MO)',
 	GingerMoPhp = 'Ginger-MO (PHP)',
-	ObjectCache = 'Object Cache',
-	ObjectCacheL10n = 'Object Cache + L10n',
+	CacheL10n = 'Cache translations',
 	NativeGettext = 'Native Gettext',
 }
 
@@ -31,97 +30,111 @@ describe( 'Server Timing - Twenty Twenty-Three', () => {
 	} );
 
 	describe.each( [
-		{ locale: 'en', scenario: Scenario.Default },
-		{ locale: 'de_DE', scenario: Scenario.Default },
-		{ locale: 'en', scenario: Scenario.ObjectCache },
-		{ locale: 'de_DE', scenario: Scenario.ObjectCache },
-		{ locale: 'de_DE', scenario: Scenario.ObjectCacheL10n },
-		{ locale: 'de_DE', scenario: Scenario.GingerMo },
-		{ locale: 'de_DE', scenario: Scenario.GingerMoPhp },
-		{ locale: 'de_DE', scenario: Scenario.NativeGettext },
-	] )( 'Locale: $locale, Scenario: $scenario', ( { locale, scenario } ) => {
-		beforeAll( async () => {
-			await setLocale( locale );
+		{ locale: 'en', scenario: Scenario.Default, objectCache: false },
+		{ locale: 'de_DE', scenario: Scenario.Default, objectCache: false },
+		{ locale: 'de_DE', scenario: Scenario.GingerMo, objectCache: false },
+		{ locale: 'de_DE', scenario: Scenario.GingerMoPhp, objectCache: false },
+		{
+			locale: 'de_DE',
+			scenario: Scenario.NativeGettext,
+			objectCache: false,
+		},
+		{ locale: 'en', scenario: Scenario.Default, objectCache: true },
+		{ locale: 'de_DE', scenario: Scenario.Default, objectCache: true },
+		{ locale: 'de_DE', scenario: Scenario.CacheL10n, objectCache: true },
+		{ locale: 'de_DE', scenario: Scenario.GingerMo, objectCache: true },
+		{ locale: 'de_DE', scenario: Scenario.GingerMoPhp, objectCache: true },
+		{
+			locale: 'de_DE',
+			scenario: Scenario.NativeGettext,
+			objectCache: true,
+		},
+	] )(
+		'Locale: $locale, Scenario: $scenario',
+		( { locale, scenario, objectCache } ) => {
+			beforeAll( async () => {
+				await setLocale( locale );
 
-			if ( scenario === Scenario.NativeGettext ) {
-				await activatePlugin( 'native-gettext' );
-			}
-
-			if (
-				scenario === Scenario.ObjectCache ||
-				scenario === Scenario.ObjectCacheL10n
-			) {
-				await activatePlugin( 'sqlite-object-cache' );
-			}
-
-			if ( Scenario.ObjectCacheL10n ) {
-				await activatePlugin( 'wp-performance-pack' );
-				// Enable l10n object caching in WP Performance Pack but nothing else.
-				await setOption(
-					'wppp_option',
-					'a:3:{s:21:"mod_l10n_improvements";b:1;s:14:"use_mo_dynamic";b:0;s:10:"mo_caching";b:1;}'
-				);
-			}
-
-			if (
-				scenario === Scenario.GingerMo ||
-				scenario === Scenario.GingerMoPhp
-			) {
-				await activatePlugin( 'ginger-mo' );
-			}
-
-			if ( scenario === Scenario.GingerMo ) {
-				await activatePlugin( 'ginger-mo-no-php' );
-			}
-		} );
-
-		afterAll( async () => {
-			await setLocale( 'en' );
-			await deactivatePlugin( 'ginger-mo' );
-			await deactivatePlugin( 'ginger-mo-no-php' );
-			await deactivatePlugin( 'sqlite-object-cache' );
-			await deactivatePlugin( 'native-gettext' );
-			await deactivatePlugin( 'wp-performance-pack' );
-		} );
-
-		it( 'Server Timing Metrics', async () => {
-			const result: Record< string, number[] > = {};
-
-			let i = globalThis.TEST_RUNS;
-
-			while ( i-- ) {
-				await page.goto( createURL( '/' ) );
-
-				const serverTiming: Record< string, number > =
-					await page.evaluate( () =>
-						(
-							performance.getEntriesByType(
-								'navigation'
-							) as PerformanceNavigationTiming[]
-						 )[ 0 ].serverTiming.reduce( ( acc, entry ) => {
-							acc[ entry.name ] = entry.duration;
-							return acc;
-						}, {} )
-					);
-
-				for ( const [ key, value ] of Object.entries( serverTiming ) ) {
-					if ( ! result[ key ] ) {
-						result[ key ] = [];
-					}
-					result[ key ].push( value );
+				if ( objectCache ) {
+					await activatePlugin( 'sqlite-object-cache' );
 				}
-			}
 
-			results.push( {
-				Locale: locale,
-				Scenario: scenario,
-				...Object.fromEntries(
-					Object.entries( result ).map( ( [ key, value ] ) => [
-						key,
-						median( value as number[] ),
-					] )
-				),
+				if ( scenario === Scenario.NativeGettext ) {
+					await activatePlugin( 'native-gettext' );
+				}
+
+				if ( scenario === Scenario.CacheL10n ) {
+					await activatePlugin( 'wp-performance-pack' );
+					// Enable l10n object caching in WP Performance Pack but nothing else.
+					await setOption(
+						'wppp_option',
+						'a:3:{s:21:"mod_l10n_improvements";b:1;s:14:"use_mo_dynamic";b:0;s:10:"mo_caching";b:1;}'
+					);
+				}
+
+				if (
+					scenario === Scenario.GingerMo ||
+					scenario === Scenario.GingerMoPhp
+				) {
+					await activatePlugin( 'ginger-mo' );
+				}
+
+				if ( scenario === Scenario.GingerMo ) {
+					await activatePlugin( 'ginger-mo-no-php' );
+				}
 			} );
-		} );
-	} );
+
+			afterAll( async () => {
+				await setLocale( 'en' );
+				await deactivatePlugin( 'ginger-mo' );
+				await deactivatePlugin( 'ginger-mo-no-php' );
+				await deactivatePlugin( 'sqlite-object-cache' );
+				await deactivatePlugin( 'native-gettext' );
+				await deactivatePlugin( 'wp-performance-pack' );
+			} );
+
+			it( 'Server Timing Metrics', async () => {
+				const result: Record< string, number[] > = {};
+
+				let i = globalThis.TEST_RUNS;
+
+				while ( i-- ) {
+					await page.goto( createURL( '/' ) );
+
+					const serverTiming: Record< string, number > =
+						await page.evaluate( () =>
+							(
+								performance.getEntriesByType(
+									'navigation'
+								) as PerformanceNavigationTiming[]
+							 )[ 0 ].serverTiming.reduce( ( acc, entry ) => {
+								acc[ entry.name ] = entry.duration;
+								return acc;
+							}, {} )
+						);
+
+					for ( const [ key, value ] of Object.entries(
+						serverTiming
+					) ) {
+						if ( ! result[ key ] ) {
+							result[ key ] = [];
+						}
+						result[ key ].push( value );
+					}
+				}
+
+				results.push( {
+					Locale: locale,
+					Scenario: scenario,
+					'Object Cache': objectCache,
+					...Object.fromEntries(
+						Object.entries( result ).map( ( [ key, value ] ) => [
+							key,
+							median( value as number[] ),
+						] )
+					),
+				} );
+			} );
+		}
+	);
 } );
