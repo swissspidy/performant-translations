b1f998a794d8d3613f8e7db5efd7b12cebcff846
Pascal Birchler <pascalb@google.com>
2023-06-30 09:43:17 +0200

    Collect memory usage

---
+++ tests/wp-env/mu-plugins/server-timing-memory-usage.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Plugin Name: Server Timing Memory Usage
+ * Description: Add memory usage to the Performance Lab plugin's Server Timing API.
+ * Version: 0.1.0
+ * Author: Pascal Birchler
+ * Author URI: https://pascalbirchler.com
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
+
+add_action(
+	'plugins_loaded',
+	static function() {
+		perflab_server_timing_register_metric(
+			'memory-usage',
+			array(
+				'measure_callback' => function( $metric ) {
+					$metric->set_value( memory_get_usage() );
+				},
+				'access_cap'       => 'exist',
+			)
+		);
+	}
+);
