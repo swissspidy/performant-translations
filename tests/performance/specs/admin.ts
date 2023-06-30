f29cfbcc460d3f44f652c0f118d6a3889a09164c
Pascal Birchler <pascalb@google.com>
2023-06-30 11:46:27 +0200

    Run tests in admin as well

---
+++ tests/performance/specs/admin.ts
@@ -0,0 +1,130 @@
+import {
+	activatePlugin,
+	deactivatePlugin,
+	visitAdminPage,
+} from '@wordpress/e2e-test-utils';
+import { writeFileSync } from 'node:fs';
+import { getResultsFilename, median } from '../utils';
+import { setLocale, enablePerformancePackL10n } from '../../e2e-utils';
+
+enum Scenario {
+	Default = 'Default',
+	GingerMo = 'Ginger-MO (MO)',
+	GingerMoPhp = 'Ginger-MO (PHP)',
+	CacheL10n = 'Cache translations',
+	NativeGettext = 'Native Gettext',
+}
+
+describe( 'Server Timing - WordPress Admin', () => {
+	const results = [];
+
+	afterAll( async () => {
+		const resultsFilename = getResultsFilename( __filename );
+		writeFileSync( resultsFilename, JSON.stringify( results, null, 2 ) );
+	} );
+
+	describe.each( [
+		{ locale: 'en', scenario: Scenario.Default, objectCache: false },
+		{ locale: 'de_DE', scenario: Scenario.Default, objectCache: false },
+		{ locale: 'de_DE', scenario: Scenario.GingerMo, objectCache: false },
+		{ locale: 'de_DE', scenario: Scenario.GingerMoPhp, objectCache: false },
+		{
+			locale: 'de_DE',
+			scenario: Scenario.NativeGettext,
+			objectCache: false,
+		},
+		{ locale: 'en', scenario: Scenario.Default, objectCache: true },
+		{ locale: 'de_DE', scenario: Scenario.Default, objectCache: true },
+		{ locale: 'de_DE', scenario: Scenario.CacheL10n, objectCache: true },
+		{ locale: 'de_DE', scenario: Scenario.GingerMo, objectCache: true },
+		{ locale: 'de_DE', scenario: Scenario.GingerMoPhp, objectCache: true },
+		{
+			locale: 'de_DE',
+			scenario: Scenario.NativeGettext,
+			objectCache: true,
+		},
+	] )(
+		'Locale: $locale, Scenario: $scenario',
+		( { locale, scenario, objectCache } ) => {
+			beforeAll( async () => {
+				await setLocale( locale );
+
+				if ( objectCache ) {
+					await activatePlugin( 'sqlite-object-cache' );
+				}
+
+				if ( scenario === Scenario.NativeGettext ) {
+					await activatePlugin( 'native-gettext' );
+				}
+
+				if ( scenario === Scenario.CacheL10n ) {
+					await activatePlugin( 'wp-performance-pack' );
+					await enablePerformancePackL10n();
+				}
+
+				if (
+					scenario === Scenario.GingerMo ||
+					scenario === Scenario.GingerMoPhp
+				) {
+					await activatePlugin( 'ginger-mo' );
+				}
+
+				if ( scenario === Scenario.GingerMo ) {
+					await activatePlugin( 'ginger-mo-no-php' );
+				}
+			} );
+
+			afterAll( async () => {
+				await setLocale( 'en' );
+				await deactivatePlugin( 'ginger-mo' );
+				await deactivatePlugin( 'ginger-mo-no-php' );
+				await deactivatePlugin( 'sqlite-object-cache' );
+				await deactivatePlugin( 'native-gettext' );
+				await deactivatePlugin( 'wp-performance-pack' );
+			} );
+
+			it( 'Server Timing Metrics', async () => {
+				const result: Record< string, number[] > = {};
+
+				let i = globalThis.TEST_RUNS;
+
+				while ( i-- ) {
+					await visitAdminPage( 'index.php' );
+
+					const serverTiming: Record< string, number > =
+						await page.evaluate( () =>
+							(
+								performance.getEntriesByType(
+									'navigation'
+								) as PerformanceNavigationTiming[]
+							 )[ 0 ].serverTiming.reduce( ( acc, entry ) => {
+								acc[ entry.name ] = entry.duration;
+								return acc;
+							}, {} )
+						);
+
+					for ( const [ key, value ] of Object.entries(
+						serverTiming
+					) ) {
+						if ( ! result[ key ] ) {
+							result[ key ] = [];
+						}
+						result[ key ].push( value );
+					}
+				}
+
+				results.push( {
+					Locale: locale,
+					Scenario: scenario,
+					'Object Cache': objectCache,
+					...Object.fromEntries(
+						Object.entries( result ).map( ( [ key, value ] ) => [
+							key,
+							median( value as number[] ),
+						] )
+					),
+				} );
+			} );
+		}
+	);
+} );
