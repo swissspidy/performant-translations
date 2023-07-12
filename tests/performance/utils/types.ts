4cb399f879ef1dd726611835f79cda202a80ec3c
Pascal Birchler <pascalb@google.com>
2023-07-12 10:31:13 +0200

    Update spelling

--- tests/performance/utils/types.ts
+++ tests/performance/utils/types.ts
@@ -1,9 +1,9 @@
 export enum Scenario {
 	Default = 'Default',
 	Dynamo = 'DynaMo',
-	GingerMo = 'Ginger-MO (MO)',
-	GingerMoPhp = 'Ginger-MO (PHP)',
-	GingerMoJson = 'Ginger-MO (JSON)',
+	GingerMo = 'Ginger MO (MO)',
+	GingerMoPhp = 'Ginger MO (PHP)',
+	GingerMoJson = 'Ginger MO (JSON)',
 	ObjectCache = 'Cache in object cache',
 	Apcu = 'Cache in APCu',
 	NativeGettext = 'Native Gettext',
