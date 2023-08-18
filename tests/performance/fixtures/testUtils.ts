81a518bb04c57aa8be82cac524e72976f876fd9c
Pascal Birchler <pascalb@google.com>
2023-08-18 11:17:46 +0200

    Fix performance test

--- tests/performance/fixtures/testUtils.ts
+++ tests/performance/fixtures/testUtils.ts
@@ -18,7 +18,7 @@ class TestUtils {
 			scenario === Scenario.GingerMoPhp ||
 			scenario === Scenario.GingerMoJson
 		) {
-			await this.requestUtils.activatePlugin( 'ginger-mo' );
+			await this.requestUtils.activatePlugin( 'performant-translations' );
 		}
 
 		if ( scenario === Scenario.GingerMo ) {
@@ -38,7 +38,7 @@ class TestUtils {
 			language: '',
 		} );
 
-		await this.requestUtils.deactivatePlugin( 'ginger-mo' );
+		await this.requestUtils.deactivatePlugin( 'performant-translations' );
 		await this.requestUtils.deactivatePlugin( 'ginger-mo-prefer-json' );
 		await this.requestUtils.deactivatePlugin( 'ginger-mo-prefer-mo' );
 
