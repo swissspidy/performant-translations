d947db974e7c5c7ca0b71536d3302c53c4a959c7
Pascal Birchler <pascalb@google.com>
2023-10-16 20:51:24 +0200

    Composer update

--- tests/phpstan/bootstrap.php
+++ tests/phpstan/bootstrap.php
@@ -4,7 +4,6 @@ define( 'GINGER_MO_TEST_DATA', '' );
 define( 'DIR_TESTDATA', '' );
 define( 'WP_LANG_DIR', '' );
 define( 'DIR_PLUGIN_TESTDATA', '' );
-define( 'WP_PLUGIN_DIR', '' );
 
 function i18n_plugin_test() {
 	return __( 'This is a dummy plugin', 'internationalized-plugin' );
