85b58a2215a9455b020bd7b90acba98c094d24cf
Pascal Birchler <pascalb@google.com>
2024-01-30 14:39:34 +0100

    Add polyfill to bootstrap

--- tests/phpunit/unit/bootstrap.php
+++ tests/phpunit/unit/bootstrap.php
@@ -10,3 +10,25 @@ require __DIR__ . '/includes/Plural_Forms.php';
 require __DIR__ . '/includes/Ginger_MO_TestCase.php';
 
 define( 'GINGER_MO_TEST_DATA', __DIR__ . '/data/', false );
+
+if ( ! function_exists( 'str_starts_with' ) ) {
+	/**
+	 * Polyfill for `str_starts_with()` function added in PHP 8.0.
+	 *
+	 * Performs a case-sensitive check indicating if
+	 * the haystack begins with needle.
+	 *
+	 * @since 5.9.0
+	 *
+	 * @param string $haystack The string to search in.
+	 * @param string $needle   The substring to search for in the `$haystack`.
+	 * @return bool True if `$haystack` starts with `$needle`, otherwise false.
+	 */
+	function str_starts_with( $haystack, $needle ) {
+		if ( '' === $needle ) {
+			return true;
+		}
+
+		return 0 === strpos( $haystack, $needle );
+	}
+}
