8d16b846ee450cc1506a8e9640fb6b310e5f68d8
Pascal Birchler <pascalb@google.com>
2024-04-12 23:40:48 +0200

    Remove now obsolete code (#173)

--- performant-translations.php
+++ performant-translations.php
@@ -3,14 +3,14 @@
  * Plugin Name: Performant Translations
  * Plugin URI:  https://github.com/swissspidy/performant-translations
  * Description: Making internationalization/localization in WordPress faster than ever before.
- * Version:     1.1.2
+ * Version:     1.2.0
  * Network:     true
  * Author:      WordPress Performance Team
  * Author URI:  https://make.wordpress.org/performance/
  * License:     GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain: performant-translations
- * Requires at least: 6.3
+ * Requires at least: 6.5
  * Requires PHP: 7.0
  *
  * @package Performant_Translations
@@ -20,13 +20,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-define( 'PERFORMANT_TRANSLATIONS_VERSION', '1.1.2' );
+define( 'PERFORMANT_TRANSLATIONS_VERSION', '1.2.0' );
 
-require __DIR__ . '/lib/class-ginger-mo.php';
-require __DIR__ . '/lib/class-ginger-mo-translation-file.php';
-require __DIR__ . '/lib/class-ginger-mo-translation-file-mo.php';
-require __DIR__ . '/lib/class-ginger-mo-translation-file-php.php';
-require __DIR__ . '/lib/class-performant-translations-compat-provider.php';
 require __DIR__ . '/lib/class-performant-translations.php';
 
 // All the WordPress magic.
--- tests/phpunit/integration/tests/Performant_Translations_Tests.php
+++ tests/phpunit/integration/tests/Performant_Translations_Tests.php
@@ -43,6 +43,115 @@ class Performant_Translations_Tests extends WP_UnitTestCase {
 	}
 
 	/**
+	 * @covers ::init
+	 *
+	 * @return void
+	 */
+	public function test_init() {
+		$this->assertSame( 100, has_filter( 'load_translation_file', array( Performant_Translations::class, 'load_translation_file' ) ) );
+
+		$this->assertSame( 10, has_action( 'wp_head', array( Performant_Translations::class, 'add_generator_tag' ) ) );
+		$this->assertSame( 10, has_action( 'performant_translations_file_written', array( Performant_Translations::class, 'opcache_invalidate' ) ) );
+		$this->assertSame( 10, has_action( 'upgrader_process_complete', array( Performant_Translations::class, 'upgrader_process_complete' ) ) );
+		$this->assertSame( 10, has_action( 'loco_file_written', array( Performant_Translations::class, 'regenerate_translation_file' ) ) );
+		$this->assertSame( 10, has_action( 'wpml_st_translation_file_updated', array( Performant_Translations::class, 'regenerate_translation_file' ) ) );
+	}
+
+	/**
+	 * @covers ::load_translation_file
+	 *
+	 * @return void
+	 */
+	public function test_load_translation_file_creates_and_reads_php_files_if_filtered_format_is_unsupported() {
+		add_filter(
+			'translation_file_format',
+			static function () {
+				return 'unknown-format';
+			}
+		);
+
+		$load_mo_successful = load_textdomain( 'wp-tests-domain', DIR_TESTDATA . '/pomo/simple.mo' );
+
+		$unload_mo_successful = unload_textdomain( 'wp-tests-domain' );
+
+		$load_php_successful = load_textdomain( 'wp-tests-domain', DIR_TESTDATA . '/pomo/simple.l10n.php' );
+
+		$unload_php_successful = unload_textdomain( 'wp-tests-domain' );
+
+		$this->assertTrue( $load_mo_successful, 'MO file not successfully loaded' );
+		$this->assertTrue( $unload_mo_successful );
+		$this->assertFileExists( DIR_TESTDATA . '/pomo/simple.l10n.php' );
+		$this->assertTrue( $load_php_successful, 'PHP file not successfully loaded' );
+		$this->assertTrue( $unload_php_successful );
+	}
+
+	/**
+	 * @covers ::load_translation_file
+	 *
+	 * @return void
+	 */
+	public function test_load_translation_file_creates_and_reads_php_files_no_wp_filesystem() {
+		add_filter( 'filesystem_method', '__return_empty_string' );
+
+		$load_mo_successful = load_textdomain( 'wp-tests-domain', DIR_TESTDATA . '/pomo/simple.mo' );
+
+		$unload_mo_successful = unload_textdomain( 'wp-tests-domain' );
+
+		$load_php_successful = load_textdomain( 'wp-tests-domain', DIR_TESTDATA . '/pomo/simple.l10n.php' );
+
+		$unload_php_successful = unload_textdomain( 'wp-tests-domain' );
+
+		$this->assertTrue( $load_mo_successful, 'MO file not successfully loaded' );
+		$this->assertTrue( $unload_mo_successful );
+		$this->assertFileExists( DIR_TESTDATA . '/pomo/simple.l10n.php' );
+		$this->assertTrue( $load_php_successful, 'PHP file not successfully loaded' );
+		$this->assertTrue( $unload_php_successful );
+	}
+
+	/**
+	 * @covers ::load_translation_file
+	 *
+	 * @return void
+	 */
+	public function test_load_translation_file_does_not_create_php_files_if_disabled() {
+		add_filter( 'performant_translations_convert_files', '__return_false' );
+
+		$load_mo_successful = load_textdomain( 'wp-tests-domain', DIR_TESTDATA . '/pomo/simple.mo' );
+
+		$unload_mo_successful = unload_textdomain( 'wp-tests-domain' );
+
+		$this->assertTrue( $load_mo_successful, 'MO file not successfully loaded' );
+		$this->assertTrue( $unload_mo_successful );
+		$this->assertFileDoesNotExist( DIR_TESTDATA . '/pomo/simple.l10n.php' );
+	}
+
+	/**
+	 * @param string $domain
+	 * @param string $file
+	 * @return void
+	 */
+	public function _on_load_textdomain( $domain, $file ) {
+		remove_action( 'load_textdomain', array( $this, '_on_load_textdomain' ) );
+		load_textdomain( $domain, $file );
+	}
+
+	/**
+	 * @covers ::load_translation_file
+	 *
+	 * @return void
+	 */
+	public function test_load_translation_file_inception_does_not_create_duplicate_files() {
+		add_action( 'load_textdomain', array( $this, '_on_load_textdomain' ), 10, 2 );
+
+		// Just to ensure the PHP files exist.
+		load_textdomain( 'wp-tests-domain', DIR_TESTDATA . '/pomo/simple.mo' );
+		unload_textdomain( 'wp-tests-domain' );
+
+		$this->assertFileExists( DIR_TESTDATA . '/pomo/simple.l10n.php' );
+		$this->assertFileDoesNotExist( DIR_TESTDATA . '/pomo/simple.l10n.php.php' );
+	}
+
+	/**
 	 * @covers ::upgrader_process_complete
 	 * @covers ::opcache_invalidate
 	 *
